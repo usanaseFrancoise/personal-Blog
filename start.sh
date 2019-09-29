e315cc5cc2c0cb4daefc637ae6a1733f600ab37a
usanase Francoise <fusanasencoise@gmail.com>
2019-09-29 23:16:36 +0200

    gitignore ant start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY=001
+export DATABASE_URL=postgresql+psycopg2://fanny:havugima@2019@localhost/blogs
+export MAIL_USERNAME=fusanasencoise@gmail.com
+export MAIL_PASSWORD=havugima@2019
+python3.6 manage.py run
